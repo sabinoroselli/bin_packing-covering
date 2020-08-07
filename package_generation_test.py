921edb4405953bfca124b37a74036af640583159
Fredrik Hagebring <fredrik@hagebring.com>
2020-08-07 02:58:14 +0200

    Added one more method of package generation.
    This method does not generate fit packages following the definition and strategy of the paper. Instead of filling up a package with all minimum items and then virtual items, fill it with all minimum items and if there is still space left, start filling it with the second smallest item and so on. this will ensure that it is not only the smallest item that is squeezed in but all small items. Doing this can, but not necessarily, decrease the total number of packages. It should, however, never be able to INCREASE the number so its only a potential improvement.

--- package_generation_test.py
+++ package_generation_test.py
@@ -1,16 +1,17 @@
 # from package_preprocessing import Partition, fitss, manipulatedFitss, manipulatedFitssTree
 from package_preprocessing import *
-# from gurobipy import *
+from gurobipy import *
 from time import time as tm
 import csv
 import sys
 sys.setrecursionlimit(20000)
 
-# instance = 'benchmark/test.txt'
-# instance = 'benchmark/Falkenauer_t/Falkenauer_t501_00.txt'
-instance = 'benchmark/Scholl_1/N3C1W1_A.txt'
+# instance = 'benchmark/frehage_testInstance.txt'
+# instance = 'benchmark/Falkenauer_t/Falkenauer_t60_00.txt'
+# instance = 'benchmark/Scholl_1/N3C1W1_A.txt'
+# instance = 'benchmark/Schwerin_1/Schwerin1_BPP10.txt'
 # instance = 'benchmark/Hard28/Hard28_BPP13.txt'
-# instance = 'benchmark/Wascher/Waescher_TEST0005.txt'
+instance = 'benchmark/Wascher/Waescher_TEST0022.txt'
 
 print('Package generation test')
 
@@ -31,34 +32,34 @@ print(values_classes)
 
 
 
-# OLD VERSION	
-print('')
-print('###  OLD GENERATION + MANIPULATION  ###')
+# # OLD VERSION	
+# print('')
+# print('###  OLD GENERATION + MANIPULATION  ###')
 
-# generating the fit packages
-print('')
-print('* Generating the fit packages')
-start_package_gen = tm()
-lista = fitss(values_classes.copy(), [], target)
-print('  package generation: ', tm() - start_package_gen)
-print('Packages', len(lista))
-print('Example: ', lista[0])
+# # generating the fit packages
+# print('')
+# print('* Generating the fit packages')
+# start_package_gen = tm()
+# lista = fitss(values_classes.copy(), [], target)
+# print('  package generation: ', tm() - start_package_gen)
+# print('Packages', len(lista))
+# print('Example: ', lista[0])
 
 
-# manipulating the list to make it suitable for the next steps
-print('')
-print('* Manipulating the list to make it suitable for the next steps')
-start_package_manipulation = tm()
-lista = [[x for x in classe if x[1] > 0] for classe in lista]
-lista = [{str(sub[0]): sub[1] for sub in element} for element in lista]
-print('  package manipulation: ', tm() - start_package_manipulation)
-print('Packages', len(lista))
-print('Example: ', lista[0])
-# for i in lista:
-# 	print(i)
+# # manipulating the list to make it suitable for the next steps
+# print('')
+# print('* Manipulating the list to make it suitable for the next steps')
+# start_package_manipulation = tm()
+# lista = [[x for x in classe if x[1] > 0] for classe in lista]
+# lista = [{str(sub[0]): sub[1] for sub in element} for element in lista]
+# print('  package manipulation: ', tm() - start_package_manipulation)
+# print('Packages', len(lista))
+# print('Example: ', lista[0])
+# # for i in lista:
+# # 	print(i)
 
-print('')
-print('TOTAL TIME: ', 2 * tm() - start_package_gen - start_package_manipulation)
+# print('')
+# print('TOTAL TIME: ', 2 * tm() - start_package_gen - start_package_manipulation)
 
 
 
@@ -66,15 +67,13 @@ print('TOTAL TIME: ', 2 * tm() - start_package_gen - start_package_manipulation)
 # NEW VERSION v1
 print('')
 print('###  INTEGRATED GENERATION + MANIPULATION  ###')
-
-# manipulating the list to make it suitable for the next steps
 print('')
 start_package_genm = tm()
 listam = manipulatedFitss(values_classes, target)
 print('Packages', len(listam))
 print('Example: ', listam[0])
-#for i in listam:
-#	print(i)
+for i in range(min(10,len(listam))):
+	print(listam[i])
 
 print('')
 print('TOTAL TIME: ', tm() - start_package_genm)
@@ -82,27 +81,81 @@ print('TOTAL TIME: ', tm() - start_package_genm)
 
 
 
-# NEW VERSION v2
-print('')
-print('###  STORING RESULT AS TREE  ###')
+# # NEW VERSION v2
+# print('')
+# print('###  STORING RESULT AS TREE  ###')
+# print('')
+# start_package_genm = tm()
+# treea = manipulatedFitssTree(values_classes, target)
 
 
-# manipulating the list to make it suitable for the next steps
-print('')
-start_package_genm = tm()
-treea = manipulatedFitssTree(values_classes, target)
-print('  package generation: ', tm() - start_package_genm)
 
-print('')
-print('TOTAL TIME: ', tm() - start_package_genm)
+# print('  package generation: ', tm() - start_package_genm)
+
+# print('')
+# print('TOTAL TIME: ', tm() - start_package_genm)
 
 
-# # WANNA SEE WHAT THE TREE LOOKS LIKE?
-# def exploreTree(tree,level=0):
+# # WANNA VERIFY THE NUMBER OF SOLUTIONS IN THE TREE
+# def countSolutions(tree):
 # 	if isinstance(tree,int):
-# 		print(level*"   ", tree)
-# 	else:	
-# 		for i in range(0, len(tree)):
-# 			print(level*"   ", i)
-# 			exploreTree(tree[i],level+1)
-# exploreTree(treea)
+# 		return 1
+# 	else:
+# 		return sum([countSolutions(t) for t in tree])
+# print(countSolutions(treea))
+
+
+# # # WANNA SEE WHAT THE TREE LOOKS LIKE?
+# # def exploreTree(tree,level=0):
+# # 	if isinstance(tree,int):
+# # 		print(level*"   ", tree)
+# # 	else:	
+# # 		for i in range(0, len(tree)):
+# # 			print(level*"   ", i)
+# # 			exploreTree(tree[i],level+1)
+# # exploreTree(treea)
+
+
+
+
+
+# NEW VERSION v3
+print('')
+print('###  NOVEL IDEA ON WHAT PACKAGES THAT IS REQUIRED  ###')
+print('')
+
+
+remaining_value = [sum([values_classes[i][0]*values_classes[i][1] for i in range(j+1,len(values_classes))]) 
+    for j in range(len(values_classes))]
+
+
+# An upgrade to fitss(vms, partial, target)
+# Major changes:
+# - uses indices instead of list manipulation for the vms
+# - generates the manipulated version directly, i.e. only includes nonzero classes as as dict.s
+def fitterPackageGeneration(vms, target, index=0, partial={}):
+    vm = vms[index]   # I reverse the order of pop since pop() has complexity O(1)
+    v, m = vm[0], vm[1]
+    c1 = max(0, (target - remaining_value[index]) // v)
+    c2 = min(m, target // v)
+
+    if index == len(vms) - 1:
+        # if target - m * v < vms[0][0]:
+        return [updatePartial(partial, v, c2)]
+        # else:
+        #     print(updatePartial(partial, v, c2))
+        #     return []
+    else:
+        return concat(
+            [fitterPackageGeneration(vms, target - d * v, index=index + 1, partial=updatePartial(partial, v, d)) for d in range(c1, c2 + 1)])
+
+
+start_package_genm = tm()
+listam = fitterPackageGeneration(values_classes, target)
+print('Packages', len(listam))
+print('Example: ', listam[0])
+for i in range(min(10,len(listam))):
+    print(listam[i])
+
+print('')
+print('TOTAL TIME: ', tm() - start_package_genm)
